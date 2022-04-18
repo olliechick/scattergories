a2fee52ad6c7af5068efa07841597ed88187d064
Ollie Chick <och26@uclive.ac.nz>
2022-04-18 16:14:34 +1200

    Add sounds to host page

--- src/app/host/host.component.ts
+++ src/app/host/host.component.ts
@@ -8,7 +8,7 @@ import { interval, Subscription } from "rxjs";
 })
 export class HostComponent {
 
-  readonly possibleLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U']
+  readonly possibleLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U'];
   readonly totalSeconds = 120;
 
   private subscription: Subscription | undefined;
@@ -23,14 +23,37 @@ export class HostComponent {
       this.subscription?.unsubscribe();
     } else {
       this.letter = this.possibleLetters[Math.floor(Math.random() * this.possibleLetters.length)]
+      this.playAudioFile(`${this.letter.toLowerCase()}.mp3`)
       this.seconds = this.totalSeconds - 1;
-      this.subscription = interval(100)
+      this.subscription = interval(1000)
         .subscribe(() => {
           this.seconds -= 1;
+          switch (this.seconds) {
+            case 60:
+              this.playAudioFile('one-minute-remaining.mp3');
+              break;
+            case 10:
+            case 5:
+            case 3:
+            case 2:
+            case 1:
+              this.playAudioFile(`${this.seconds}.mp3`)
+              break;
+            case 0:
+              this.playAudioFile('stop.mp3')
+              break;
+          }
           if (this.seconds <= 0) {
             this.subscription?.unsubscribe();
           }
         });
     }
+  }
+
+  playAudioFile(filename: string): void {
+    let audio = new Audio();
+    audio.src = `assets/sounds/${filename}`;
+    audio.load();
+    audio.play();
   }
 }
